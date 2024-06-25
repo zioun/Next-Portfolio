9af301e41ad2601b402a894e5eb2cf8619335063
Zioun <jionkhan0@gmail.com>
2024-06-26 05:24:28 +0600

    complete

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
